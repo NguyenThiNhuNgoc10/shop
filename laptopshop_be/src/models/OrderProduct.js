b80964c5d52cfab98d19148dae1ec853740ef0e1
NguyenThiNhuNgoc10 <nhungoc4188@gmail.com>
2023-10-24 22:52:20 +0700

    update

---
+++ laptopshop_be/src/models/OrderProduct.js
@@ -0,0 +1,39 @@
+
+const mongoose = require("mongoose");
+const orderSchema = new mogoose.Schema({
+    orderItems: [
+        {
+            name: { type: String, required: true },
+            amount: { type: Number, required: true },
+            image: { type: String, required: true },
+            price: { type: Number, required: true },
+            product: {
+                type: mogoose.Schema.Types.ObjectId,
+                ref: 'Product',
+                required: true,
+            },
+        },
+    ],
+    shippingAddress: {
+        fullName: { type: String, required: true },
+        address: { type: String, required: true },
+        city: { type: String, required: true },
+        phone: { type: Number, required: true },
+    },
+    paymentMethod: { type: String, required: true},
+    itemsPrice: { type: Number, required: true },
+    shippingPrice: { type: Number, required: true },
+    taxPrice: { type: Number, required: true },
+    totalPrice: { type: Number, required: true },
+    use: { type: mogoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    ispaid: { type: Boolean, default: false },
+    paidAt: { type: Date },
+    isDelivered: { type: Boolean, default: false },
+    deliveredAt: { type: Date },
+},
+    {
+        timstamps: true,
+    }
+);
+const Order = mogoose.model('Order', orderSchema); 
+module.exports = Order
